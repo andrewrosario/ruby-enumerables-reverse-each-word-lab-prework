5c44a77bc6f65cc2cd8c5ea41aea208b7a98e968
Andrew Rosario <rosario.andrew@gmail.com>
2019-06-14 20:09:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,4 +6,15 @@ def reverse_each_word(string)
   p memo.join(" ")
 end
 
+def reverse_each_word(string)
+  memo = []
+  string.split(' ').each { |word|
+    memo << word.reverse
+  }
+  p memo.join(" ")
+end
+
+
+
+
 reverse_each_word("This is a long list of words")
